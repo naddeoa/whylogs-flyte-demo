ce481bd601e8ae659a2ef0811452ec52dcc41991
Anthony Naddeo <anthony@whylabs.ai>
2022-05-24 15:41:59 -0700

    Updated transformer

--- flyte/workflows/example.py
+++ flyte/workflows/example.py
@@ -22,7 +22,7 @@ class WhylogsDatasetProfileTransformer(TypeTransformer[DatasetProfileView]):
     """
 
     # TODO Spark plugin is a SchemaType. How do I pick? Is blob right?
-    _TYPE_INFO = BlobType(format="binary", dimensionality=BlobType.BlobDimensionality.MULTIPART)
+    _TYPE_INFO = BlobType(format="binary", dimensionality=BlobType.BlobDimensionality.SINGLE)
 
     def __init__(self):
         super(WhylogsDatasetProfileTransformer, self).__init__("whylogs-profile-transformer", t=DatasetProfileView)
@@ -44,15 +44,15 @@ class WhylogsDatasetProfileTransformer(TypeTransformer[DatasetProfileView]):
         return Literal(scalar=Scalar(blob=Blob(uri=remote_path, metadata=BlobMetadata(type=self._TYPE_INFO))))
 
     def to_python_value(self, ctx: FlyteContext, lv: Literal, expected_python_type: Type[DatasetProfileView]) -> T:
-        # TODO Do I need to write it locally or can I just keep it in memory?
         local_dir = ctx.file_access.get_random_local_path()
-        ctx.file_access.download_directory(lv.scalar.blob.uri, local_dir)
+        ctx.file_access.download(lv.scalar.blob.uri, local_dir)
         return DatasetProfileView.read(local_dir)
 
     # TODO where is this visible from?
     # TODO how can I test this out locally? Do I need to build a wheel of flytekit and use it from a real project?
     def to_html(self, ctx: FlyteContext, python_val: DatasetProfileView, expected_python_type: Type[DatasetProfileView]) -> str:
         return str(python_val.to_pandas().to_html())
+
 
 TypeEngine.register(WhylogsDatasetProfileTransformer())
 ## DELETE START
